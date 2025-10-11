708c75531418f47ed84a841499f0da4622ae6e4d
kn <fangshenghui@gmail.com>
2025-10-11 23:46:32 +0800

    feat:小程序开发

--- source/community_service/community_service/views.py
+++ source/community_service/community_service/views.py
@@ -6,5 +6,5 @@ def welcome(request):
     # 查询出 order 最大的一个记录
     resp = Welcome.objects.all().order_by('-order').first()
     img = 'http://127.0.0.1:8000/media/' + str(resp.img)
-    return JsonResponse({'code': 200, 'msg': '成功', 'result': img})
+    return JsonResponse({'code': 0, 'msg': '成功', 'result': img})
 
--- source/wx-miniprogram-community/pages/welcome/welcome.js
+++ source/wx-miniprogram-community/pages/welcome/welcome.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    second:5
+    second:3,
+    bgImg: "/images/bg/welcome.png"
   },
 
   doJump(){
@@ -19,22 +20,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-   
+    console.log('welcome页面加载完成')
+    
+    // 请求后端接口，加载背景图
+    wx.request({
+      url: 'http://127.0.0.1:8000/community/welcome',
+      method: 'GET',
+      success: (res) => {
+        console.log(res)
+        if (res.data.code == 0) {
+          this.setData({
+            bgImg: res.data.result
+          })
+        }else {
+          wx.showToast({
+            title: "请求网络异常",
+          })
+        }
+      }
+    })
+  
+    
     var inst = setInterval(()=>{
       // 先倒计时
       this.setData({
         second: this.data.second - 1
       })
       
+      console.log('当前倒计时:', this.data.second)
+      
       // 再判断是否需要跳转
       if (this.data.second <= 0){
+        console.log('倒计时结束，准备跳转')
+        
         // 清除定时器
         clearInterval(inst)
 
-        // 跳转
-        wx.switchTab({
-          url: '/pages/index/index',
-        })
+        // 延迟一点再跳转，确保页面状态更新完成
+        setTimeout(() => {
+          console.log('开始跳转到首页')
+          wx.switchTab({
+            url: '/pages/index/index',
+            success: function(res) {
+              console.log('跳转成功', res)
+            },
+            fail: function(err) {
+              console.error('跳转失败', err)
+            }
+          })
+        }, 100)
       }
     }, 1000)
   },
